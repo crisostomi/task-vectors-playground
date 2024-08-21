71532fbe0a1e674142d807845f2ff7c01ee55f88
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2024-08-21 16:19:05 +0200

    set model name according to conf in evaluate.py

--- src/scripts/evaluate.py
+++ src/scripts/evaluate.py
@@ -147,7 +147,7 @@ def run(cfg: DictConfig) -> str:
     #artifact_name = f"{cfg.nn.module.model.model_name}_2stOrderUnifiedModel_{cfg.seed_index}"
     artifact_name = f"{cfg.nn.module.model.model_name}_One{epoch_divisor}Eps{order}{num_to_th[order]}OrderUnifiedModel_{cfg.seed_index}"
     #artifact_name = f"{cfg.nn.module.model.model_name}_HalfEpsSomeDatasets2ndOrderUnifiedModel_{cfg.seed_index}" #################
-    metadata = {"model_name": "ViT-B-16", "model_class": "tvp.modules.encoder.ImageEncoder"}
+    metadata = {"model_name": f"{cfg.nn.module.model.model_name}", "model_class": "tvp.modules.encoder.ImageEncoder"}
     upload_model_to_wandb(task_equipped_model, artifact_name, logger.experiment, cfg, metadata)
 
 
